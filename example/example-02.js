ff08cdbd87398fdd525d03dda5e9c5e92783aa13
Florian Völker <Neirolf2030@web.de>
2019-01-11 22:52:23 +0100

    changed second example to match the first

--- example/example-02.js
+++ example/example-02.js
@@ -1,11 +1,18 @@
 const { ODSConnection } = require('ods-api');
 
 ODSConnection.login('username', 'password')
-    .then(conn => conn == null ? null : Promise.all([
-        conn.getMetaInformation(),
-        conn.getGradesSummary(),
-        conn.getGrades()
-    ]))
+    .then(conn => {
+        if (conn == null)
+            throw new Error('either username and / or password was wrong or the the ods service was not reachable');
+        else
+            return Promise.all([
+                conn.getMetaInformation(),
+                conn.getGradesSummary(),
+                conn.getGrades()
+
+            ]);
+    })
     .then(([metaInformation, gradesSummy, grades]) => {
         require('fs').writeFileSync(metaInformation.matriculationNumber + '.json', JSON.stringify({ metaInformation, gradesSummy, grades }, undefined, 4))
-    }, console.error);
+    })
+    .catch(console.error);
